f894e94918360b06a6854224137821168e1f6c1b
Dev Shah <devshah1032@gmail.com>
2021-06-23 18:26:28 +0530

    lel

--- main.js
+++ main.js
@@ -53,3 +53,21 @@ function timer(){
 function stopInterval() {
     clearInterval(startTimer);
 }
+function requestFullScreen(element) {
+    // Supports most browsers and their versions.
+    var requestMethod = element.requestFullScreen || element.webkitRequestFullScreen || element.mozRequestFullScreen || element.msRequestFullScreen;
+
+    if (requestMethod) { // Native full screen.
+        requestMethod.call(element);
+    } else if (typeof window.ActiveXObject !== "undefined") { // Older IE.
+        var wscript = new ActiveXObject("WScript.Shell");
+        if (wscript !== null) {
+            wscript.SendKeys("{F11}");
+        }
+    }
+}
+function fullscreen(){
+    var elem = document.body; // Make the body go full screen.
+    requestFullScreen(elem);
+}
+
